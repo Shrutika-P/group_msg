fa4e13a7f5edec9773e4c2d2d58f2313fe814552
Shrutika Patil <shrutika.patil@northgateps.com>
2021-09-16 01:41:59 +0530

    few application level changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,17 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+
+// this code has to be corrected
+submit_message = function () {
+    $('#message_body').on('keydown', function (e) {
+        if (e.key == 13) {
+            //  $('button').click();
+            e.target.value = "";
+        };
+    });
+};
+
 scroll_bottom = function () {
     if ($('#messages').length > 0) {
         $('#messages').scrollTop($('#messages')[0].scrollHeight);
@@ -26,6 +37,6 @@ $(document).on('turbolinks:load', function () {
     $('.message .close').on('click', function () {
         $(this).closest('.message').transition('fade');
     });
-
+    submit_message();
     scroll_bottom();
 })
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
 
     belongs_to :user
     validates :body, Presence:true
+
+    scope :custom_display, -> { order(:created_at).last(5) }
 end
